181b9c6eb09a03f6621532077fccccb6ff86a989
zezutom <zezulatomas@gmail.com>
2015-07-02 19:33:11 +0200

    Simple app demo: input validation and docs

--- src/main/scala/org/zezutom/learningspark/SimpleApp.scala
+++ src/main/scala/org/zezutom/learningspark/SimpleApp.scala
@@ -1,20 +1,39 @@
 package org.zezutom.learningspark
 
-import org.apache.spark.{SparkContext, SparkConf}
+import java.io.File
+
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * A simple app demo.
  *
- * Note that applications should define a main() method instead of extending scala.App. Subclasses of scala.App may not work correctly.
+ * Note that applications should define a main() method instead of extending scala.App.
+ * Subclasses of scala.App may not work correctly.
+ *
+ * How to Run:
+ *
+ * $SPARK_HOME/bin/spark-submit \
+ * --class "org.zezutom.learningspark.SimpleApp" \
+ * --master local[4] \
+ * target/learningspark-1.0-SNAPSHOT.jar README.md
+ * 
  */
 object SimpleApp {
   def main(args: Array[String]): Unit = {
-    val logFile = args{0}
+    if (args.length == 0)
+      throw new IllegalArgumentException("Please provide a file to parse")
+
+    val logFile = args(0)
+
+    if (!new File(logFile).exists())
+      throw new IllegalArgumentException(s"No such file: $logFile")
+
     val conf = new SparkConf().setAppName("Simple App")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
+
     println("Lines with a: %s, Lines with b: %b".format(numAs, numBs))
   }
 }
