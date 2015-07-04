290786d0331116d4a413938b71266335d2d39d1e
zezutom <zezulatomas@gmail.com>
2015-07-04 16:59:43 +0200

    WordCount as a standalone class in Java

--- src/main/java/org/zezutom/learningspark/WordCount.java
+++ src/main/java/org/zezutom/learningspark/WordCount.java
@@ -1,6 +1,5 @@
 package org.zezutom.learningspark;
 
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -9,35 +8,22 @@ import scala.Tuple2;
 import java.io.File;
 import java.util.Arrays;
 
-/**
- * Counts the number of words in the given file and saves the result into an output directory.
- *
- * How to Run:
- *
- * $SPARK_HOME/bin/spark-submit \
- * --class "org.zezutom.learningspark.WordCount" \
- * --master local[4] \
- * target/learningspark-1.0-SNAPSHOT.jar src/main/resources/README.md
- *
- */
 public class WordCount {
 
-    public static void main(String[] args) {
-        if (args.length == 0)
-            throw new IllegalArgumentException("Please provide a file to parse");
+    private final JavaSparkContext sc;
 
-        String file = args[0];
+    public WordCount(JavaSparkContext sc) {
+        this.sc = sc;
+    }
 
+    public JavaPairRDD<String, Integer> wc(String file) {
         if (!new File(file).exists())
             throw new IllegalArgumentException("No such file: " + file);
 
-        SparkConf conf = new SparkConf().setAppName("Word Count");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> data = sc.textFile(file, 2);
+        return wc(sc.textFile(file, 2));
+    }
 
-        JavaPairRDD<String, Integer> wc;
-        wc = data.flatMap(s -> Arrays.asList(s.split(" "))).mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((i1, i2) -> i1 + i2);
-
-        wc.saveAsTextFile("wc_out.txt");
+    public JavaPairRDD<String, Integer> wc(JavaRDD<String> lines) {
+        return lines.flatMap(s -> Arrays.asList(s.split(" "))).mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((i1, i2) -> i1 + i2);
     }
 }
---
+++ src/main/java/org/zezutom/learningspark/WordCountApp.java
@@ -0,0 +1,28 @@
+package org.zezutom.learningspark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * Counts the number of words in the given file and saves the result into an output directory.
+ *
+ * How to Run:
+ *
+ * $SPARK_HOME/bin/spark-submit \
+ * --class "org.zezutom.learningspark.WordCountApp" \
+ * --master local[4] \
+ * target/learningspark-1.0-SNAPSHOT.jar src/main/resources/README.md
+ *
+ */
+public class WordCountApp {
+
+    public static void main(String[] args) {
+        if (args.length == 0)
+            throw new IllegalArgumentException("Please provide a file to parse");
+
+        SparkConf conf = new SparkConf().setAppName("Word Count");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        new WordCount(sc).wc(args[0]).saveAsTextFile("wc_out.txt");
+    }
+}
