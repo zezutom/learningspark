09b3e19b90d6421f5fd22962a21494c1800d9486
zezutom <zezulatomas@gmail.com>
2015-07-04 14:35:08 +0200

    Java WordCount - code cleanup and docs

--- src/main/java/org/zezutom/learningspark/WordCount.java
+++ src/main/java/org/zezutom/learningspark/WordCount.java
@@ -4,9 +4,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 import java.io.File;
@@ -20,7 +17,7 @@ import java.util.Arrays;
  * $SPARK_HOME/bin/spark-submit \
  * --class "org.zezutom.learningspark.WordCount" \
  * --master local[4] \
- * target/learningspark-1.0-SNAPSHOT.jar README.md
+ * target/learningspark-1.0-SNAPSHOT.jar src/main/resources/README.md
  *
  */
 public class WordCount {
@@ -38,22 +35,8 @@ public class WordCount {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> data = sc.textFile(file, 2);
 
-        JavaPairRDD<String, Integer> wc = data.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public Iterable<String> call(String s) throws Exception {
-                return Arrays.asList(s.split(" "));
-            }
-        }).mapToPair(new PairFunction<String, String, Integer>() {
-            @Override
-            public Tuple2<String, Integer> call(String s) throws Exception {
-                return new Tuple2<String, Integer>(s, 1);
-            }
-        }).reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
-            public Integer call(Integer i1, Integer i2) throws Exception {
-                return i1 + i2;
-            }
-        });
+        JavaPairRDD<String, Integer> wc;
+        wc = data.flatMap(s -> Arrays.asList(s.split(" "))).mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((i1, i2) -> i1 + i2);
 
         wc.saveAsTextFile("wc_out.txt");
     }
