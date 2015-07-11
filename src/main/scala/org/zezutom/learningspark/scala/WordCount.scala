16f36eea7cfafe55b8ac9013ce0ebbdeb9ec77c0
zezutom <zezulatomas@gmail.com>
2015-07-11 20:42:38 +0200

    Scala upgraded to 2.11, WordCount unit tests

--- src/main/scala/org/zezutom/learningspark/scala/WordCount.scala
+++ src/main/scala/org/zezutom/learningspark/scala/WordCount.scala
@@ -4,6 +4,9 @@ import org.apache.spark.rdd.RDD
 
 object WordCount {
   def wc(lines:RDD[String]): RDD[(String, Int)] = {
-    lines.flatMap(line => line.split(" ")).filter(!_.isEmpty).map(word => (word, 1)).reduceByKey(_ + _)
+    lines.flatMap(_.split("\\s"))                             // Split on any white character
+      .filter(!_.isEmpty)                                     // Only count words as non-empty strings
+      .map(_.stripSuffix(",").stripSuffix(".").toLowerCase)   // Remove punctuation and make all words lowercase
+      .map(word => (word, 1)).reduceByKey(_ + _).sortBy(_._1)
   }
 }
--- src/test/scala/org/zezutom/learninspark/WordCountSpec.scala
+++ src/test/scala/org/zezutom/learninspark/WordCountSpec.scala
@@ -19,4 +19,21 @@ class WordCountSpec extends FlatSpec with SparkSpec with GivenWhenThen with Matc
     Then("empty count")
     wordCounts shouldBe empty
   }
+
+  "Robert Frost's famous quote" should "be counted" in {
+    Given("quote")
+    val lines = Array(
+      "Two roads diverged in a wood, and I,",
+      "I took the one less traveled by",
+      "And that has made all the difference.")
+
+    When("count words")
+    val wordCounts = WordCount.wc(sc.parallelize(lines)).collect()
+
+    Then("words counted")
+    wordCounts should equal(Array(
+      ("a", 1), ("all", 1), ("and", 2), ("by", 1), ("difference", 1), ("diverged", 1),
+      ("has", 1), ("i", 2), ("in", 1), ("less", 1), ("made", 1), ("one", 1),
+      ("roads", 1), ("that", 1), ("the" ,2), ("took" ,1), ("traveled" ,1), ("two", 1), ("wood", 1)))
+  }
 }
